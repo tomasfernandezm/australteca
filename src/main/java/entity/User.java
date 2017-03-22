e83548a2f9a7e022f5fc3906c26c6ca54ef4eae4
Tomas Fernandez <tomas.martinez@ing.austral.edu.ar>
2017-03-22 20:21:04 -0300

    Added user

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,7 +1,78 @@
 package entity;
 
+import javax.persistence.*;
+
 /**
  * Created by tomi on 22/03/17.
  */
+
+@Entity
+@Table(name = "USER")
 public class User {
+
+    @Id @GeneratedValue
+    @Column(name = "ID")
+    private int id;
+
+    @Column(name = "FNAME")
+    private String firstName;
+
+    @Column(name = "LNAME")
+    private String lastName;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "COURSE")
+    private String course;
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String email, String course) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
 }
